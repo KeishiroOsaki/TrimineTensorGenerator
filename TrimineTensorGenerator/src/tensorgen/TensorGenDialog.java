766e24afdf1b03192858d5a8d60ab8d71ed81b67
oosakikeishiro <startipend2008@gmail.com>
2015-10-21 19:12:39 +0900

    テンプレだけ作った

---
+++ TrimineTensorGenerator/src/tensorgen/TensorGenDialog.java
@@ -0,0 +1,43 @@
+package tensorgen;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+class TensorGenDialog {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TensorGenDialog window = new TensorGenDialog();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public TensorGenDialog() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
---
+++ TrimineTensorGenerator/src/tensorgen/TensorGenerator.java
@@ -0,0 +1,5 @@
+package tensorgen;
+
+public class TensorGenerator {
+
+}
