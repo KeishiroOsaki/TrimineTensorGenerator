60ceb5cf92949f5ea31447b41e2e5847cd209c11
KeishiroOsaki <startipend2008@gmail.com>
2015-10-26 23:59:06 +0900

    OAstructを作った

---
+++ TrimineTensorGenerator/src/tensorgen/OAstruct.java
@@ -0,0 +1,15 @@
+package tensorgen;
+
+class OAstruct {
+	String object;
+	String actor;
+	int value;
+	
+	public OAstruct(String object, String actor, int value) {
+		// TODO 自動生成されたコンストラクター・スタブ
+		this.object = object; 
+		this.actor = actor;
+		this.value = value;
+	}
+
+}
