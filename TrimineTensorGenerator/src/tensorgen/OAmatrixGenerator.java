610b7b675f4d2ea7ff529fecfa1e4909756af4aa
KeishiroOsaki <startipend2008@gmail.com>
2015-11-14 00:03:55 +0900

    オブジェクトの生成回数を抑制

--- TrimineTensorGenerator/src/tensorgen/OAmatrixGenerator.java
+++ TrimineTensorGenerator/src/tensorgen/OAmatrixGenerator.java
@@ -8,7 +8,7 @@ import java.util.List;
 import org.apache.commons.collections4.map.MultiKeyMap;
 
 class OAmatrixGenerator implements Runnable {
-	
+
 	private static final String SLASH = " / ";
 	private static final String TIME_N2 = " time_n = ";
 	private static final String TENSOR_GEN_OUTPUT_FILE_NAME = "TensorGen outputFileName = ";
@@ -25,11 +25,10 @@ class OAmatrixGenerator implements Runnable {
 	private static final String colone = ":";
 	private static final String space = " ";
 	private boolean taskdone = false;
-	
-	
-	OAmatrixGenerator(String outputFileName, long time_n, List<String> object,
-			List<String> actor, DataDAO dbCon, ProgressFrame progressFrame) {
-		
+
+	OAmatrixGenerator(String outputFileName, long time_n, List<String> object, List<String> actor, DataDAO dbCon,
+			ProgressFrame progressFrame) {
+
 		this.outputFileName = outputFileName;
 		this.time_n = time_n;
 		this.object = object;
@@ -37,11 +36,9 @@ class OAmatrixGenerator implements Runnable {
 		this.dbCon = dbCon;
 		this.progressFrame = progressFrame;
 		this.objectsize = object.size();
-		
-		
+
 	}
 
-	
 	@Override
 	public void run() {
 		// TODO 自動生成されたメソッド・スタブ
@@ -49,63 +46,69 @@ class OAmatrixGenerator implements Runnable {
 		int nowprogres = 0;
 		progressFrame.addList(status_sb);
 		setStatus_sb(nowprogres);
-		
+
 		File file = new File(outputFileName);
 		try {
-			
+
 			FileWriter filewriter = new FileWriter(file, true);
 			filewriter.write(object.size() + " " + actor.size() + " 0\n");
-			
+
 			MultiKeyMap<String, Integer> res = dbCon.sqlExecute(time_n);
-			
-			
-			
-			for (int i = 0; i < object.size()  && !Thread.currentThread().isInterrupted()/* && !TensorGenerator.exec.isShutdown()*/; i++) {
-				oneLineGenerate(filewriter, res, i);
+
+			StringBuilder ressb = new StringBuilder(space);
+
+			for (int i = 0; i < object.size() && !Thread.currentThread()
+					.isInterrupted()/* && !TensorGenerator.exec.isShutdown() */; i++) {
+				oneLineGenerate(filewriter, res, i, ressb);
 				nowprogres++;
 				setStatus_sb(nowprogres);
-				//System.out.println(Thread.currentThread().isInterrupted());
-				
+				// System.out.println(Thread.currentThread().isInterrupted());
+
 			}
-			
+
 			filewriter.close();
-			res=null;
+			res = null;
 			progressFrame.progvalIncrement();
 			progressFrame.removeList(status_sb);
 
-			
 		} catch (IOException e) {
 			// TODO 自動生成された catch ブロック
 			e.printStackTrace();
 		}
-		processing=false;
+		processing = false;
 		setTaskdone(true);
 	}
 
-
-	private void oneLineGenerate(FileWriter filewriter, MultiKeyMap<String, Integer> res, int i) throws IOException {
+	private void oneLineGenerate(FileWriter filewriter, MultiKeyMap<String, Integer> res, int i, StringBuilder ressb)
+			throws IOException {
 		int elenum = 0;
-	    StringBuilder ressb = new StringBuilder(space);
+
+		ressb.delete(0, ressb.length());
+		ressb.append(space);
 		for (int j = 0; j < actor.size(); j++) {
-			
+
 			Integer k = res.get(object.get(i), actor.get(j));
 			if (k != null) {
 				elenum++;
-				ressb.append(j+1 + colone + k + space);
-			}					
+				ressb.append(j + 1 + colone + k + space);
+			}
 		}
 		ressb.insert(0, elenum);
 		ressb.append(terminalStr);
 		filewriter.write(ressb.toString());
 	}
 
-
 	/**
 	 * @param nowprogres
 	 */
 	private void setStatus_sb(int nowprogres) {
 		status_sb.delete(0, status_sb.length());
-		status_sb.append(TENSOR_GEN_OUTPUT_FILE_NAME/* + outputFileName + TIME_N2 + time_n + space + nowprogres + SLASH + objectsize*/);
+		status_sb.append(
+				TENSOR_GEN_OUTPUT_FILE_NAME/*
+											 * + outputFileName + TIME_N2 +
+											 * time_n + space + nowprogres +
+											 * SLASH + objectsize
+											 */);
 		status_sb.append(outputFileName);
 		status_sb.append(TIME_N2);
 		status_sb.append(time_n);
@@ -113,9 +116,8 @@ class OAmatrixGenerator implements Runnable {
 		status_sb.append(nowprogres);
 		status_sb.append(SLASH);
 		status_sb.append(objectsize);
-		//System.out.println(status_sb);
+		// System.out.println(status_sb);
 	}
-
 
 	/**
 	 * @return taskdone
@@ -124,9 +126,9 @@ class OAmatrixGenerator implements Runnable {
 		return taskdone;
 	}
 
-
 	/**
-	 * @param taskdone セットする taskdone
+	 * @param taskdone
+	 *            セットする taskdone
 	 */
 	void setTaskdone(boolean taskdone) {
 		this.taskdone = taskdone;
